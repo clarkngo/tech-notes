0a287f84400eb3f0fe37b1a09551917723645f4a
Clark Ngo <clarkngo@gmail.com>
2019-04-29 12:44:52 -0700

    create hash table in java

---
+++ java/HashTable.java
@@ -0,0 +1,31 @@
+import java.util.*; 
+
+public class HashTable {
+  public static void main(String[] args) {
+    // create a table and add some values 
+    Map<String, Integer> table = new Hashtable<>(); 
+    table.put("Pen", 10); 
+    table.put("Book", 100); 
+
+    // print map details 
+    System.out.println("hashTable: "
+                      + table.toString()); 
+
+    // provide value for new key which is absent
+
+    // using computeIfAbsent method 
+    table.computeIfAbsent("newPen", k -> 600); 
+    table.computeIfAbsent("newBook", k -> 800); 
+
+    // using computeIfPresent method
+    table.computeIfPresent("Pen", (k,v) -> v + 1); 
+
+    // print new mapping 
+    System.out.println("new hashTable: "
+                      + table); 
+
+    table.clear();
+    System.out.println("empty hashTable: "
+                      + table); 
+  }
+}
